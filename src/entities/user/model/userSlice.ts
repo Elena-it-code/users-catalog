e836b817c0f14d9a0bd2bfa6433f3a14427e8333
ElenaKuklina <cuklina.lena@gmail.com>
2026-03-16 21:55:55 +0300

    feat(user): add async thunk for fetching users and extraReducers

--- src/entities/user/model/userSlice.ts
+++ src/entities/user/model/userSlice.ts
@@ -1,5 +1,7 @@
 import type { User } from '@/entities';
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 export type UserState = {
   users: User[];
@@ -13,10 +15,29 @@ const initialState: UserState = {
   error: null,
 };
 
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await fetch(`${BASE_URL}/users`);
+  return await response.json();
+});
+
 export const userSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message || 'Failed to fetch users';
+      });
   },
 });
 
