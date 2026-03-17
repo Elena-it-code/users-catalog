c74f07c648a8007ebb4aee88dc2395c6db446ae2
ElenaKuklina <cuklina.lena@gmail.com>
2026-03-17 17:23:42 +0300

    feat: add user profile page

--- src/pages/user-page/ui/UserPage.tsx
+++ src/pages/user-page/ui/UserPage.tsx
@@ -1,7 +1,26 @@
+import type { RootState } from '@/app/store.ts';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
 export const UserPage = () => {
+  const users = useSelector((state: RootState) => state.users.users);
+  const { id } = useParams();
+  const userId = Number(id);
+
+  const user = users.find((u) => u.id === userId);
+
+  if (!user) {
+    return <h1>User not found</h1>;
+  }
+
   return (
     <div>
-      <h1>Full user information</h1>
+      <h1>{user.name}</h1>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+      <p>Company: {user.company.name}</p>
+      <p>City: {user.address.city}</p>
     </div>
   );
 };
