f63986f6662f3f7f6e2734b112ce08e571ecd9b0
ElenaKuklina <cuklina.lena@gmail.com>
2026-03-17 18:31:37 +0300

    feat: add user search and sorting logic

--- src/pages/main-page/ui/MainPage.tsx
+++ src/pages/main-page/ui/MainPage.tsx
@@ -1,7 +1,7 @@
 import { UserList } from '@/widgets/user-list';
 import { useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch, RootState } from '@/app/store.ts';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchUsers } from '@/entities/user/model/userSlice.ts';
 
 export const MainPage = () => {
@@ -9,6 +9,17 @@ export const MainPage = () => {
   const { users, isLoading, error } = useSelector(
     (state: RootState) => state.users,
   );
+
+  const [search, setSearch] = useState('');
+  const [sort, setSort] = useState('');
+
+  const filteredUsers = users
+    .filter((u) => u.name.toLowerCase().includes(search.toLowerCase()))
+    .sort((a, b) => {
+      if (sort === 'asc') return a.name.localeCompare(b.name);
+      if (sort === 'desc') return b.name.localeCompare(a.name);
+      return 0;
+    });
 
   useEffect(() => {
     if (users.length === 0) {
@@ -21,7 +32,19 @@ export const MainPage = () => {
 
   return (
     <div>
-      <UserList users={users} />
+      <input
+        placeholder="Search by name..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <select value={sort} onChange={(e) => setSort(e.target.value)}>
+        <option value="">Sort by</option>
+        <option value="asc">Name (A-Z)</option>
+        <option value="desc">Name (Z-A)</option>
+      </select>
+
+      <UserList users={filteredUsers} />
     </div>
   );
 };
